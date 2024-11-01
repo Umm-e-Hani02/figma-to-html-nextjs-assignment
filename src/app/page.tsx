f339c35a20ee3e7fb16438d764e1492028133316
ummehani <hanirh2468@gmail.com>
2024-11-01 15:45:01 +0500

    Implement figma to html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Blog from "./components/blog";
 import Featured from "./components/Feature";
 import Footer from "./components/Footer";
 
-export default function Home() {
-  return (
-   <div>
-    <Navbar/>
-    <Hero/>
-    <Blog/>
-    <Featured/>
-    <Footer/>
-   </div>
-  );
+export default function Home(){
+  return(
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Featured/>
+      <Footer/>
+    </div>
+  )
 }
